75a29e2a4ff6c83e8d326aa5e775604b74c6f5de
101045330 <101045330@georgebrown.ca>
2024-10-15 17:49:44 +0000

    cleared issues after removing some lines from main.mjs

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -14,7 +14,7 @@ const list = [
 
 
 // Navigate the page to a URL.
-await page.goto('https://101045330.github.io/desn3035-e5');//redirection
+await page.goto('https://101045330.github.io/DESN3035-E5/');//redirection
 
 // Set screen size.
 //await page.setViewport({width: 1080, height: 1024});//dimension of viewport
@@ -25,25 +25,11 @@ await page.screenshot({
     path: "auto_screenshot.png"
 })
 // Type into search box.
-await page.locator('.devsite-search-field').fill('automate beyond recorder');//find a section and ass search string
-
-// Wait and click on first result.
-await page.locator('.devsite-result-item-link').click();
-
-// Locate the full title with a unique string.
-const textSelector = await page
-  .locator('text/Customize and automate')
-  .waitHandle();
-const fullTitle = await textSelector?.evaluate(el => el.textContent);//get,set title
-
-// Print the full title.
-console.log('The title of this blog post is "%s".', fullTitle);//print title
-
 
 const options = {
     output: 'html'
 };
-const runnerResult = await lighthouse('https://101045330.github.io/desn3035-e5', options, undefined,page);
+const runnerResult = await lighthouse('https://101045330.github.io/DESN3035-E5/', options, undefined,page);
 
 
 const reportHtml = runnerResult.report;
