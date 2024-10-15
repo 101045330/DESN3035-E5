305ce92e7154b2f882f70e08498bf333189cb0ae
chriskgbc <148381481+chriskgbc@users.noreply.github.com>
2024-10-15 04:09:01 +0000

    Image update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
                 <Nav />
 
                 <div className="col-9">
-                    <img className="d-block w-100 mb-3" src="https://fastly.picsum.photos/id/251/1280/720.jpg?hmac=YDyCcHsQDDzvzCfZ-MLi2fr2pFqpoH9OqSqmJFBwV7w" />
+                    <img className="d-block w-100 mb-3" src="https://picsum.photos/id/501/1280/720" />
                     <h1>About Us</h1>
                     <p>
                         We are the best coffee roaster!
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
                 <Nav />
 
                 <div className="col-9">
-                    <img className="d-block w-100 mb-3" src="https://fastly.picsum.photos/id/251/1280/720.jpg?hmac=YDyCcHsQDDzvzCfZ-MLi2fr2pFqpoH9OqSqmJFBwV7w" />
+                    <img className="d-block w-100 mb-3" src="https://picsum.photos/id/301/1280/720" />
                     <h1>Contact Us</h1>
                     <p>
                         You can reach us at 647 XXX YYYY.
